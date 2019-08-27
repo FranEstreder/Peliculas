c1245face337b40370ec1263a4eba1c41d38cc8c
Fran <festreder@customerville.com>
2019-08-27 12:20:27 +0200

    no message

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -5,5 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class MovieService {
 
+  url = 'https://api.themoviedb.org/3/';
+  apiKey = 'a2644ef202fe255c2e1539db9fe81e6d';
+
   constructor() { }
 }
